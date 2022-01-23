504352f7ba83ccf5157fcd03dc15444392f5e1dc
Kakashi <nickperel@live.com>
2022-01-22 22:55:37 -0500

    added the ability to exit the application from the main prompt, finished writing the logic for all of the view choices including viewDepartments, viewRoles, and viewEmployees

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const mainPrompt = () => {
     type: 'list',
     name: 'action',
     message: 'What would you like to do?',
-    choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+    choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'exit the application']
 })
 .then((answers) => {
     switch(answers.action){
@@ -43,20 +43,48 @@ const mainPrompt = () => {
         case 'update an employee role':
             updateRole();
             break;
+        case 'exit the application':
+            console.log('Come back soon!')
+            process.exit(0);
         };
     });
 };
 
 const viewDepartments = () => {
-    connection.promise().query('SELECT * FROM department')
-    .then(([rows]) => {
-        console.table(rows);
+    const sql = `SELECT * FROM department`
+
+    connection.query(sql, (err, res) => {
+        if (err) console.log({ error: err.message });
+        console.table(res);
         mainPrompt();
     });
-    // connection.query("SELECT * FROM department", (err, result) => {
-    //     console.table(result)
-    // })
-};
+}
+
+const viewRoles = () => {
+    const sql = `SELECT role.id AS role_id, role.title AS job_title, role.salary, department.name AS department
+                 FROM role
+                 LEFT JOIN department ON role.department_id = department.id`;
+
+    connection.query(sql, (err, res) => {
+        if (err) console.log({ error: err.message });
+        console.table(res);
+        mainPrompt();
+    });
+}
+
+const viewEmployees = () => {
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary AS salary, employee.manager_id AS manager
+                 FROM employee
+                 LEFT JOIN employee m ON employee.manager_id = m.id
+                 LEFT JOIN role ON employee.role_id = role.id
+                 LEFT JOIN department ON role.department_id = department.id`;
+
+    connection.query(sql, (err, res) => {
+        if (err) console.log({ error: err.message });
+        console.table(res);
+        mainPrompt();
+    });
+} 
 
 const addDepartment = () => {
 
